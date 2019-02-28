85753f372306fe6f41d49a28ff38c02c3331d3f3
mpmcauliffe <mcauliffe99@gmail.com>
2019-02-27 22:04:57 -0500

    connect to mongoDB

--- app.js
+++ app.js
@@ -1,37 +1,67 @@
 const express           = require('express')
       bodyParser        = require('body-parser')
       graphqlHttp       = require('express-graphql') 
+      mongoose          = require('mongoose')
 
 const { buildSchema }   = require('graphql')
 
 const app               = express()
 
 
+const events = []
+
+
 app.use(bodyParser.json())
 app.use('/graphql', graphqlHttp({
     schema: buildSchema(`
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
         type RootQuery {
-            events: [String!]!,
+            events: [Event!]!
         }
         type RootMutation {
-            createEvent(name: String): String,
+            createEvent(eventInput: EventInput): Event
         }
         schema {
-            query: RootQuery,
-            mutation: RootMutation,
+            query: RootQuery
+            mutation: RootMutation
         }
     `),
     rootValue: {
         events: () => {
-            return ['Comb the bear', 'All-Night Coding', 'Feed the Gods to Kronos',]
+            return events
         },
         createEvent: (args) => {
-            const eventName = args.name
-            return eventName
+            const event = {
+                _id: Math.random().toString(),
+                title: args.eventInput.title,
+                description: args.eventInput.description,
+                price: +args.eventInput.price,
+                date: args.eventInput.date,
+            }
+            events.push(event)
+            return event
         },
     },
     graphiql: true,
 }))
 
 
-app.listen(3000, () => { console.log('up on 3000') })
+mongoose.connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-kng3h.mongodb.net/test?retryWrites=true`
+).then(() => {
+    app.listen(3000, () => { console.log('up on 3000') })
+}).catch(err => {
+    console.log(err)
+})
