9a8b0da45be60706877520ba291533913c18447b
mpmcauliffe <mcauliffe99@gmail.com>
2019-03-06 14:17:23 -0500

    update middleware to allowow request from client

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -3,16 +3,51 @@ import './auth.css'
 
 
 class AuthPage extends Component {
+    constructor(props) {
+        super(props)
+        this.emailElement = React.createRef()
+        this.passwordElement = React.createRef()
+    }
+    submitHandler = e => {
+        e.preventDefault()
+        const email = this.emailElement.current.value
+        const password = this.passwordElement.current.value
+
+        if(email.trim().length === 0 || password.trim().length === 0) {
+            return
+        }
+
+        const requestBody = {
+            query: `
+                mutation {
+                    createUser(userInput: {email: "${email}", password: "${password}"}) {
+                        _id
+                        email
+                    }
+                }
+            `
+        }
+
+        fetch('http://localhost:3001/graphql', {
+            method: 'POST',
+            body: JSON.stringify(requestBody),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+    }
+
+
     render() {
         return (
-            <form className='auth-form'>
+            <form className='auth-form' onSubmit={this.submitHandler}>
                 <div className='form-control'>
                     <label htmlFor='email'>email</label>
-                    <input type='email' id='email' />
+                    <input type='email' id='email' ref={this.emailElement} />
                 </div>
                 <div className='form-control'>
                     <label htmlFor='password'>password</label>
-                    <input type='password' id='password' />
+                    <input type='password' id='password' ref={this.passwordElement} />
                 </div>
                 <div className='form-actions'>
                     <button type='submit'>Submit</button>
